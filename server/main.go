4c1b9a9f5bd1eb42586339374da187d3a473c682
Alex <ansofianidis@outlook.com>
2023-05-27 22:13:13 -0500

    continued setup for go backend

--- server/main.go
+++ server/main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/joho/godotenv"
+
+	"log"
 )
 
 func main() {
@@ -14,9 +17,13 @@ func main() {
 		AllowOrigins: "http://localhost:5173",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
-
 	app.Use(logger.New())
 	app.Use(recover.New())
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Err loading .env file")
+	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
